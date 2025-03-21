8f59167c0e9c2eeb035d647b2e22d3ab86cc39f9
Lars Pieters <lars.pieters@student.howest.be>
2025-03-21 16:09:39 +0100

    added states

--- app/layouts/sidebars.jsx
+++ app/layouts/sidebars.jsx
@@ -1,25 +1,56 @@
-
 import { Outlet, useLocation } from "react-router-dom";
 import "./leftsidebar.css";
 import "./rightsidebar.css";
-
+import { useState } from "react";
 
 const LeftSidebar = () => {
     const location = useLocation();
-    const table = location.state || { name: "Select a table" };
+    const table = location.state || { name: "Select a table", id: 0 };
+    const [items, setItems] = useState({});
 
+    const predefinedItems = [
+        { id: 1, name: "Coca Cola" },
+        { id: 2, name: "Sprite" },
+        { id: 3, name: "Fanta" },
+        { id: 4, name: "Ice Tea" },
+    ];
 
+    const updateItemCount = (itemId) => {
+        setItems((prevItems) => ({
+            ...prevItems,
+            [table.id]: {
+                ...prevItems[table.id],
+                [itemId]: (prevItems[table.id]?.[itemId] || 0) + 1,
+            },
+        }));
+    };
+
+    const tableItems = items[table.id] || {};
 
     return (
         <div className="app-layout">
             <div className="leftsidebar">
                 <h1 className="table--name">{table.name}</h1>
+                <div className="item-buttons">
+                    {predefinedItems.map((item) => (
+                        <button
+                            key={item.id}
+                            onClick={() => updateItemCount(item.id)}
+                        >
+                            {item.name}
+                            <span>
+                                {" "}
+                                {tableItems[item.id] || 0}
+                            </span>
+                        </button>
+                    ))}
+                </div>
             </div>
             <main className="app-main">
                 <Outlet />
             </main>
             <div className="rightsidebar">
-                <h1>order:</h1>
+                <h1>Order:</h1>
             </div>
         </div>
     );
